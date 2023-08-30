59773e3ed0c93478a96ab6cb992eb4f20d40f8ad
Ralph Kesrewani <ralph.kesrewani@wojo.com>
2023-08-30 15:48:05 +0200

    testing change

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,5 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.title('Wojo Streamlit Demo')
-
-print('Hello, world!')
+st.title('Cette application a été construite localement sur le PC de Ralph Kesrewani')
+st.title('Florian, VTF')
