ada63ea8181aa54d1e9b3fd3ab4700544ac3fe80
Ralph Kesrewani <ralph.kesrewani@wojo.com>
2023-08-30 15:50:09 +0200

    testing changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,5 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.title('Cette application a été construite localement sur le PC de Ralph Kesrewani')
-st.title('Florian, VTF')
+st.title('Cette application python a été construite localement sur le PC de Ralph Kesrewani')
+st.title('va te faire foutre Adrian')
